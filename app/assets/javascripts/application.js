622457eb0cbb692ecd8a8eca305a5e1967d4b878
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-29 18:21:06 +0000

    automatically backed up by learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require wrong_place_wrong_time
+= require jquery
+= require jquery_ujs
+= require turbolinks
+= require wrong_place_wrong_time
